c2f67a97579f413c5b5bb4d45453f2682d261137
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2023-11-22 22:37:49 -0500

    Added a footer section

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,1 +1,51 @@
-
+<footer class="flex flex-col py-20 sm:py-32 bg-black border border-t border-solid border-violet-950 gap-4 sm:gap-8 items-center justify-center">
+    <p class="px-4 py-2 bg-white text-slate-400 font-medium">Connect with me &rarr;</p>
+    <div class="flex flex-col gap-4 items-center justify-center border-b border-dashed pb-4">
+        <p class="">
+            <b class="pr-2">Email:</b>
+            <a 
+                href="mailto:guhan.delta@gmail.com" 
+                class="text-violet-400"
+                target="_blank"
+            >
+                Guhaprasaanth<sup>
+                    <span class="text-xs scale-75 pl-0.5">
+                        <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]" />
+                    </span>
+                </sup>
+            </a>
+        </p>
+    </div>
+    <div class="flex flex-col gap-4 items-center justify-center -mt-4">
+        <p class="">
+            <b class="pr-2">LinkedIn:</b>
+            <a 
+                href="https://www.linkedin.com/in/guhaprasaanthnandagopal/" 
+                class="text-violet-400"
+                target="_blank"
+            >
+                Guhaprasaanth<sup>
+                    <span class="text-xs scale-75 pl-0.5">
+                        <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]" />
+                    </span>
+                </sup>
+            </a>
+        </p>
+    <!-- </div>
+    <div class="flex flex-col gap-4 items-center justify-center"> -->
+        <p class="">
+            <b class="pr-2">Github:</b>
+            <a 
+                href="https://www.github.com/guhandelta/" 
+                class="text-violet-400"
+                target="_blank"
+            >
+                GuhaN<sup>
+                    <span class="text-xs scale-75 pl-0.5">
+                        <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]" />
+                    </span>
+                </sup>
+            </a>
+        </p>
+    </div>
+</footer>
